4be7278af4ac145a17d969ed6dda5f53d3cea5d3
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-07-10 04:14:41 +0530

    Markers Added in the correct location of device

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ if(navigator.geolocation){
     }
 );
 }
-const map=L.map("map").setView([0,0],10);
+const map=L.map("map").setView([0,0],16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution:"LiveTrace"
@@ -26,4 +26,10 @@ const markers={ };
 socket.on("recieve-location",(data)=>{
     const{id,latitude,longitude}=data;
     map.setView([latitude,longitude]);
+    if(markers[id]){
+        markers[id].setLatLang([latitude,longitude]);
+    }
+    else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map);
+    }
 })
